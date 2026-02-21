d0d6b2b86884773c589eaa2783ded4feb283f18c
AcidPlant <acidplayer200775@gmail.com>
2026-02-21 16:58:29 +0500

    g

--- migrations/000069_extend_payments_for_robokassa_flows.down.sql
+++
@@ -1,7 +0,0 @@
-DROP INDEX IF EXISTS uq_payments_inv_id;
-
-ALTER TABLE payments
-    DROP COLUMN IF EXISTS response_package,
-    DROP COLUMN IF EXISTS inv_id,
-    DROP COLUMN IF EXISTS plan,
-    DROP COLUMN IF EXISTS type;
